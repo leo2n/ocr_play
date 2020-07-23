16909a920a68560fb1c616f883ed6c125b423a87
leo2n <ttxs.an@gmail.com>
2020-07-24 01:59:47 +0800

    add:数据库只能本地连接

--- mysql/dockerScript/runmysqlOCR.sh
+++ mysql/dockerScript/runmysqlOCR.sh
@@ -3,7 +3,7 @@
 docker stop ocrmysql && docker rm ocrmysql
 docker rmi ocrmysql:test
 docker build -t ocrmysql:test .
-
 docker network create ocr # 创建这个应用的专属 network bridge ocr
-docker run -d --name ocrmysql -p 3310:3306 -v $HOME/teletraan/mysql/conf.d:/etc/mysql/conf.d -v $HOME/teletraan/mysql/data:/var/lib/mysql -v $PWD/initScripts:/docker-entrypoint-initdb.d --network=ocr   leo2n/ocrmysql:test
+
+docker run -d --name ocrmysql -p 127.0.0.1:3310:3306 -v $HOME/teletraan/mysql/conf.d:/etc/mysql/conf.d -v $HOME/teletraan/mysql/data:/var/lib/mysql -v $PWD/initScripts:/docker-entrypoint-initdb.d --network=ocr   leo2n/ocrmysql:test
 # 在ocr网络中, 找到数据库的对应IP地址, 填写到应用的配置文件中
