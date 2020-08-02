5a7d3201777ebd597e4de3797bea95855d96c19b
leo2n <ttxs.an@gmail.com>
2020-08-03 04:24:28 +0800

    change path from /home/leo to /home/leo/docker_container

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ go build -o teletraanBin *.go
 docker stop ocrapp && docker rm ocrapp;
 docker rmi leo2n/ocrapp:test;
 docker build -t leo2n/ocrapp:test . ;
-docker run -d --name ocrapp --restart=always -p 4001:4001 -v $HOME/teletraan/imageStore:/usr/local/teletraan/imageStore --network=ocr leo2n/ocrapp:test ;
+docker run -d --name ocrapp --restart=always -p 4001:4001 -v $HOME/docker_container/teletraan/imageStore:/usr/local/teletraan/imageStore --network=ocr leo2n/ocrapp:test ;
--- mysql/dockerScript/runmysqlOCR.sh
+++ mysql/dockerScript/runmysqlOCR.sh
@@ -5,5 +5,5 @@ docker rmi ocrmysql:test
 docker build -t ocrmysql:test .
 docker network create ocr # 创建这个应用的专属 network bridge ocr
 
-docker run -d --name ocrmysql -p 127.0.0.1:3310:3306 --restart=always -v $HOME/teletraan/mysql/conf.d:/etc/mysql/conf.d -v $HOME/teletraan/mysql/data:/var/lib/mysql -v $PWD/initScripts:/docker-entrypoint-initdb.d --network=ocr --net-alias=ocrmysql leo2n/ocrmysql:test
+docker run -d --name ocrmysql -p 127.0.0.1:3310:3306 --restart=always -v $HOME/docker_container/teletraan/mysql/conf.d:/etc/mysql/conf.d -v $HOME/docker_container/teletraan/mysql/data:/var/lib/mysql -v $PWD/initScripts:/docker-entrypoint-initdb.d --network=ocr --net-alias=ocrmysql leo2n/ocrmysql:test
 # 在ocr网络中, 找到数据库的对应IP地址, 填写到应用的配置文件中
